06da3e0fb3ed6d457dc952df06d8804308b437d6
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2026-08-10 09:38:33 +0700

    a

--- app/controller/peminjamcontroller.php
+++ app/controller/peminjamcontroller.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../model/peminjam.php';
+require_once __DIR__ . '/../model/peminjaman.php';
 
 class PeminjamController{
     private $db;
