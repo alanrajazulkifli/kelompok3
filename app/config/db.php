be1d2902a2f68e1f777d91e6da37c57f27026cf1
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2026-08-10 09:20:51 +0700

    a

--- app/config/db.php
+++ app/config/db.php
@@ -1,3 +1,27 @@
 <?php
+class database {
+    private  $host = "localhost";
+    private  $db = "db_perpustakaan";
+    private  $username = "root";
+    private  $pw = "rpl12345";
+    private  $conn = null;
 
+    public function getConnection(): ?PDO {
+        if ($this->conn === null) {
+            try {
+              $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db,
+                $this->username,
+                $this->pw
+              );
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                echo json_encode(["error" => "koneksi database gagal" . $e->getMessage()]);
+                exit;
+            }
+        }
+        return $this->conn;
+    }
+}
 ?>
